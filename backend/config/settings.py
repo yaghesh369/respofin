8f42c6bc101748afa446b4780dae2aab1ca7c2e8
yaghesh369 <vyasyaghesh001@gmail.com>
2026-01-09 15:34:58 +0530

    done online db connection and customer models migrations to db

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,22 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env") 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r5)d$ro9tf=ne)pfre4_u#m#+j9)kdebqi&1+iw@ysw3mp&+##'
+print("DATABASE_URL =", os.getenv("DATABASE_URL"))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DEBUG") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
@@ -82,10 +86,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 from datetime import timedelta
 
--- backend/customers/admin.py
+++ backend/customers/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "age",
+        "income",
+        "credit_score",
+        "segment_label",
+        "created_at",
+    )
+    search_fields = ("name", "email")
+    list_filter = ("segment_label", "gender")
