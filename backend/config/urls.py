231df92fbfbadebe826a9f47659b9a3f32a6bf47
yaghesh369 <vyasyaghesh001@gmail.com>
2026-02-16 22:14:54 +0530

    small additions in tokens

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view()),
     path("api/customers/", include("customers.urls")),
     path("api/segmentation/", include("segmentation.urls")),
     path("api/recommendations/", include("recommendations.urls")),
